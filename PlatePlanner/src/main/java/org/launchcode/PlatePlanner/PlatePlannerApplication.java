323435becb259a07a710832e839cfd66ca032b90
werfelmann <werfelmann@gmail.com>
2024-12-07 15:43:43 -0600

    Spring Boot Project Initialized

---
+++ PlatePlanner/src/main/java/org/launchcode/PlatePlanner/PlatePlannerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.PlatePlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatePlannerApplication.class, args);
+	}
+
+}

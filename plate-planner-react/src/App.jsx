336e535ca51857e298670b65cc7adbf9fdb6c597
humphrey <james.humphrey@gmail.com>
2024-12-20 16:08:00 -0600

    Added React recipe text search component.

--- plate-planner-react/src/App.jsx
+++ plate-planner-react/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header.jsx';
 import RecipeList from './components/RecipeList.jsx';
+import SearchForm from './components/RecipeTextSearch.jsx';
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='App'>
       <Header />
       <RecipeList />
+      <SearchForm />
     </div>
   )
 }
---
+++ plate-planner-react/src/components/RecipeTextSearch.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function SearchForm () {
+return (
+        <div>
+            <h1>Recipe Search</h1>
+        </div>
+        );
+}
+
+export default SearchForm;
+

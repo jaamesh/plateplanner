69d17e3b0c8317f8891dbcb5ff7841e938466fb5
werfelmann <werfelmann@gmail.com>
2025-01-18 15:46:51 -0600

    Spring Security Integrated with User

--- PlatePlanner/src/main/java/org/launchcode/PlatePlanner/model/RegisterDto.java
+++ PlatePlanner/src/main/java/org/launchcode/PlatePlanner/model/RegisterDto.java
@@ -12,6 +12,17 @@ public class RegisterDto {
     @NotEmpty
     private  String lastName;
 
+    @NotEmpty
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
     public String getFirstName() {
         return firstName;
     }
